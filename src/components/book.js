5643161f3f5470db06246a7a8477eaee97bfadc0
cwelfley01 <cwelfley01@gmail.com>
2022-07-12 20:43:09 -0400

    adding image

--- src/components/book.js
+++ src/components/book.js
@@ -2,11 +2,17 @@ import React from 'react';
 
 export default function Book(props) {
   return (
-    <div>
+    <div className="book-card">
+      <div className="book-image">
+        <img src="placeholder" />
+      </div>
+      <div className="book-info-container">
         <h2>{props.bookData.name}</h2>
         <h3>{props.bookData.author}</h3>
-        <p>{props.bookData.price}</p>
         <h5>{props.bookData.description}</h5>
+        <p>{props.bookData.price}</p>
+        
+      </div>
     </div>
   )
 }
