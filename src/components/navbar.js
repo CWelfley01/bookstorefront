b65a379d8f984b96afb7003378d9f2b9939aaaae
cwelfley01 <cwelfley01@gmail.com>
2022-07-13 22:44:53 -0400

    updates

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -4,7 +4,9 @@ import { NavLink } from "react-router-dom";
 export default function Navbar() {
   return (
     <div className="navbar">
-      <h1 className="navbar-title">My Book Collection!</h1>
+      <div className="navbar-title">
+        <h1>My Book Collection!</h1>
+      </div>
       <div className="navbar-links">
         <NavLink exact className="navbar-link" activeClassName="selected" to="/">
           Home
